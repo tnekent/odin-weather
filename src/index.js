4cbb91d5054f6c182953c9eed6e1fbb8e5ba0003
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2026-01-07 08:59:13 +0800

    Add loading indicator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { domOverview, updateOverview } from "./overview.js";
 import { domDaily, updateDaily } from "./daily.js";
 import { domSearch, onQuery } from "./search.js";
+import "./index.css";
 import "./reset.css";
 
 const VC_KEY = "KGX6UFJV28G2DRT4M4XHNBWTL";
@@ -16,18 +17,23 @@ async function getWeatherData(location) {
   return data.json();
 }
 
+const pLoadingText = document.createElement("p");
+pLoadingText.classList.add("loading");
+pLoadingText.textContent = "Loading...";
+
 document.body.classList.add("initial");
 document.body.append(domSearch);
 
 onQuery((query) => {
+  document.body.replaceChildren(domSearch, pLoadingText);
   getWeatherData(query)
     .then((res) => {
       updateOverview(res);
       updateDaily(res);
+      document.body.replaceChildren(domSearch, domOverview, domDaily);
     })
-    .catch(console.error);
+    .catch(alert);
 });
 onQuery(() => {
-  document.body.append(domOverview, domDaily);
   document.body.classList.remove("initial");
 }, true);
